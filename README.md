5f7cabade33901f3e70a7b625b3fd5aff3d6840b
pauloalexandreanjos <paulo.alexandre.anjos@gmail.com>
2019-09-03 17:14:42 -0300

    Rearranjado organização de grupos que mantém quais produtos estão dentro do grupo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DividirConta
+
+Programa para dividir a conta com seus amigos num churrasco
--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var app = new Vue({
         itens: [
             {id:1, nome:"JOGOS", valor:12.0, quempagou: 1, quemusou: [1,2,3,4,5]},
             {id:2, nome:"COCA-COLA", valor:12.0, quempagou: 5, quemusou: [1,2,3,4,5]},
-            {id:3, nome:"CACHORRO-QUENTE", valor:12.0, quempagou: 3, quemusou: [1,2,3,4,5]}
+            {id:3, nome:"CACHORRO-QUENTE", valor:12.0, quempagou: 3, quemusou: [1,2,3,4,5]},
+			{id:4, nome:"CERVEJA", valor:50.0, quempagou: 2, quemusou: [1,2,4]}
         ],
         pessoas: [
-            {id:1, nome:"PAULO", checked: false},
-            {id:2, nome:"FABIO", checked: false},
-            {id:3, nome:"CRISTOPHER", checked: false},
-            {id:4, nome:"CRISTIANO", checked: false},
-            {id:5, nome:"ALAN", checked: false},
+            {id:1, nome:"PAULO", checked: false, deve:{} },
+            {id:2, nome:"FABIO", checked: false, deve:{}},
+            {id:3, nome:"CRISTOPHER", checked: false, deve:{}},
+            {id:4, nome:"CRISTIANO", checked: false, deve:{}},
+            {id:5, nome:"ALAN", checked: false, deve:{}},
         ],
         indexPessoa: 1,
         inputPessoaNome: "",
@@ -25,7 +26,8 @@ var app = new Vue({
             valor: "",
             quempagou: 0,
             quemusou: []
-        }
+        },
+		grupos: {}
     },
     methods: {
         adicionarPessoa() {
@@ -45,6 +47,8 @@ var app = new Vue({
                 app.inputItem.quemusou.push(element.id);
             });
             
+			this.inputItem.quemusou = this.inputItem.quemusou.sort(function(a,b) { return a > b ? 1 : -1 });
+			
             this.itens.push({ 
                 id: this.indexItem,
                 nome: this.inputItem.nome,
@@ -64,13 +68,16 @@ var app = new Vue({
                 return pessoa.id == id;
             })[0];
         },
+        getItem(id) {
+            return this.itens.filter(function(item) {
+                return item.id == id;
+            })[0];
+        },
         formatItens(arr) {
             let ret = "";
             let pessoas = this.pessoas;
             let app = this;
             arr.forEach(function(item) {
-                //console.log(this);
-                //var pess = pessoas.filter(function(pessoa) { return pessoa.id == item; })[0];
                 var pess = app.getPessoa(item);
                 if (ret != "") {
                     ret = ret + " / ";
@@ -78,6 +85,46 @@ var app = new Vue({
                 ret = ret + pess.nome;
             });
             return ret;
-        }
-    }
+        },
+		atualizarGrupos() {
+			this.itensGrupos = [];
+			app = this;
+			this.itens.forEach(function(item) {
+				let chave = item.quemusou.join("/");
+				if (!app.grupos[chave]) {
+					app.grupos[chave] = [];
+				}
+				//console.log("Pushing item",item.id,"into group",chave);
+				app.grupos[chave].push(item.id);
+			});
+		},
+		/*obterItensGrupo(chave) {
+			//return this.itensGrupos;
+			return this.grupos.filter(function(elm) {
+				return elm.chave == chave;
+			})
+			.map(function(elm) {
+				return elm.idItem;
+			});
+		},*/
+		obterValorGrupo(chave) {
+			let itens = this.grupos[chave].itens;
+			console.log(JSON.stringify(itens));
+			let app = this;
+			return itens.reduce(function(total, item) {
+				console.log("Reducing",item,"valor",app.getItem(item).valor);
+				return total + app.getItem(item).valor;
+			}, 0);
+		}
+    },
+	watch: {
+		itens: this.atualizarGrupos,
+		grupo: function() {
+			
+		}
+	},
+	created() {
+		this.atualizarGrupos();
+		console.log(this.obterValorGrupo("1/2/3/4/5"));
+	}
 }).$mount("#app");
